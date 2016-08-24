078fc4609721360cc0d36b0fd46052da0df79f99
Igor Amorim Silva <igoramorimsilva2@gmail.com>
2016-08-24 14:35:48 -0300

    Novo

--- src/index.js
+++ src/index.js
@@ -16,8 +16,8 @@ function onClosed() {
 
 function createMainWindow() {
 	const win = new electron.BrowserWindow({
-		width: 600,
-		height: 400
+		width: 900,
+		height: 800
 	});
 
 	win.loadURL(`file://${__dirname}/index.html`);
