c99615c087b2d6cf536afc78c445bf06b6fac4e4
Igor Amorim Silva <igoramorimsilva2@gmail.com>
2016-08-25 00:54:07 -0300

    Corrigido

--- src/Scripts/Pattern.js
+++ src/Scripts/Pattern.js
@@ -2,13 +2,36 @@
  * Created by igora on 24/08/2016.
  */
 module.exports.listPatterns = function() {
-
+	var p = require('path');
 	var fs = require('fs');
 	var patternsDir = process.cwd() + "/Patterns";
+	var i = 0;
+	var walk = require('walkdir');
+	var dirNames = fs.readdirSync(patternsDir, {options: '..', encoding: 'utf8'});
 
-	var data = fs.readdirSync(patternsDir, {options: '..', encoding: 'utf8'});
+	var raw_data = walk.sync(patternsDir,{"return_object":true},function(path,stat){
+		// ignore all .git directories.
+		if(p.basename(path) === '.git') {
+			this.ignore(path);
+		}
 
-	return data;
+	});
+
+
+	var data = {};
+
+	
+
+	while(i<dirNames.length){
+		if(dirNames[i]==='.git'){
+			dirNames[i] = dirNames[i+1];
+
+		}
+
+		data = {DirName:dirNames[i],Files: []}
+		i++;
+	}
+return data;
 }
 
 
